7abafdb4590c85e0f927b09f4f5bf6a6cab169a7
Lasse Jul-Larsen <lassejl@gmail.com>
2014-06-26 22:31:25 +0200

    Tried hibernate

--- src/main/java/no/iskald/prim/model/image/Album.java
+++ src/main/java/no/iskald/prim/model/image/Album.java
@@ -1,4 +1,54 @@
 package no.iskald.prim.model.image;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table( name = "Albums" )
 public class Album {
+    private Long id;
+    private String title;
+
+    @OneToMany(mappedBy="album")
+    private List<Entry> entries;
+
+    public Album(String title) {
+        this.title = title;
+    }
+
+    public Album() {
+    }
+
+    @Id
+    @GeneratedValue(generator="increment")
+    @GenericGenerator(name="increment", strategy = "increment")
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<Entry> getEntries() {
+        return entries;
+    }
+
+    public void setEntries(List<Entry> entries) {
+        if (this.entries == null) {
+            this.entries = new ArrayList<Entry>();
+        }
+        this.entries.addAll(entries);
+    }
 }
--- src/main/java/no/iskald/prim/model/image/Entry.java
+++ src/main/java/no/iskald/prim/model/image/Entry.java
@@ -1,9 +1,62 @@
 package no.iskald.prim.model.image;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
 import java.util.Date;
 
+@Entity
+@Table( name = "Entries" )
 public class Entry {
-    private Long key;
+    private Long id;
     private Date created;
     private String title;
+
+    @ManyToOne
+    @JoinColumn(name="id")
+    private Album album;
+
+    public Entry(Date created, String title) {
+        this.created = created;
+        this.title = title;
+    }
+
+    public Entry() {
+    }
+
+    @Id
+    @GeneratedValue(generator="increment")
+    @GenericGenerator(name="increment", strategy = "increment")
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+
+    public Album getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(Album album) {
+        this.album = album;
+    }
 }
